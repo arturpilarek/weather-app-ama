c1af223f522be2e2fe0551705b9195116c2019ed
Artur Pilarek <artur.pilarek@gmail.com>
2022-11-02 09:21:50 +0100

    Header elements development

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,14 @@
 const api_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/kolding?unitGroup=metric&key=FU7XFQM4A2CKKUUT4JXHP2EX5&contentType=json"
+const searchInput = document.getElementById("searchInput")
 
-const createApiQuery = (location) => {
-    return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=FU7XFQM4A2CKKUUT4JXHP2EX5&contentType=json`
+searchInput.addEventListener("keypress", (event) => {
+   if  (event.key === 'Enter') {
+       console.log("hi")
+   }
+})
+
+const createSimpleApiQuery = (location) => {
+    return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=us&elements=datetime%2Cname%2Caddress%2Ctemp%2Cfeelslike%2Chumidity%2Csnow%2Cwindspeed%2Cpressure%2Csolarenergy%2Csunrise%2Csunset%2Cconditions%2Cdescription%2Cicon&key=FU7XFQM4A2CKKUUT4JXHP2EX5&contentType=json`
 }
 
 async function getApiData(apiUrl) {
@@ -9,4 +16,4 @@ async function getApiData(apiUrl) {
     return await response.json()
 }
 
-getApiData(createApiQuery('kolding')).then((res) => console.log(res)).catch(err => console.log(err))
+getApiData(createSimpleApiQuery('kolding')).then((res) => console.log(res)).catch(err => console.log(err))
