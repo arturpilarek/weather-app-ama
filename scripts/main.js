49d2e979cd1705d8e47e074cb78aa4f51dcbb0ed
munaawad <munaawad@gmail.com>
2022-11-03 09:59:05 +0100

    Js conflict resolved after adding nextdaysweather section

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,34 @@ async function getApiData(apiUrl) {
 const createSimpleApiQuery = (location) => {
     return `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=metric&elements=datetime%2Cname%2Caddress%2Ctemp%2Cfeelslike%2Cdew%2Cprecip%2Csnow%2Cwindspeed%2Csunrise%2Csunset%2Cconditions%2Cdescription%2Cicon&key=FU7XFQM4A2CKKUUT4JXHP2EX5&contentType=json`
 }
+function createSixDayForcast (days){
+    let output = document.getElementById("nextdayweather")
+
+    output.innerHTML = ''
+
+    for(i =0;i < days.length;i++) {
+        output.innerHTML+=`<div class='nextdayweather__info'> 
+        <div class='icon-wrapper'>
+        <img src='./Assets/icons/weather-icons/${days[i].icon}.png'>
+    </div>
+        <div class='parameter-wrapper'>
+        <img src='./Assets/icons/weather-icons/temperature-icon.svg'>
+        <p>${days[i].temp}° <span class='small-text'>- føles som ${days[i].feelslike}°</span></p>
+    </div>
+    <div class='parameter-wrapper'>
+        <img src='./Assets/icons/weather-icons/rain-icon.svg'>
+        <p>${days[i].precip} <span class='small-text'>mm</span></p>
+    </div>
+    <div class='parameter-wrapper'>
+        <img src='./Assets/icons/weather-icons/wind-icon.svg'>
+        <p>${convertKmphToMps(days[i].windspeed)} <span class='small-text'>m/s</span></p>
+    </div> 
+        </div>`
+
+    }
+}
+     
+   
 
 const createErrorWindow = (err) => {
     const outputContainer = document.getElementById("current-weather")
@@ -32,6 +60,7 @@ const processApiData = (location) => {
         .then((res) => {
             console.log(res)
             createBasicWeatherDisplay(res)
+            createSixDayForcast(res.days)
         })
         .catch(err => createErrorWindow(err))
 
@@ -49,3 +78,4 @@ detailsToggleIcon.addEventListener("click", () => {
     detailsToggleIcon.classList.toggle('header__details-toggle--detailed')
     detailsToggleIcon.classList.toggle('header__details-toggle--simple')
 })
+
